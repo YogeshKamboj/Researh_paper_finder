e8252e9d4180473adf7c4f31033c2fe371c6505a
YogeshKamboj <97336920+YogeshKamboj@users.noreply.github.com>
2024-11-02 12:58:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def index():
     return render_template('index.html')  # Render the form on GET request
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
